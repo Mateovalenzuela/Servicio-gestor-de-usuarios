9e2f549b38619b6755f9e31aa5789edff12427a9
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2024-03-20 23:56:44 -0300

    env: Implementación de generación automatica de claves rsa al iniciar el proyecto

--- djangoProject/setings/jwt_setings.py
+++ djangoProject/setings/jwt_setings.py
@@ -9,39 +9,42 @@ load_dotenv()
 
 
 # Función para generar una clave RSA y guardarla en un archivo PEM
-def generate_rsa_key(filename_private, filename_public):
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
+def generate_rsa_key():
+    try:
+        # Generar un par de claves (clave privada y pública) para RSA
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
 
-    # Guardar la clave privada en un archivo PEM
-    with open(filename_private, 'wb') as f:
-        f.write(private_key.private_bytes(
+        private_pem = private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
-        ))
+        )
 
-    # Obtener la clave pública
-    public_key = private_key.public_key()
-
-    # Guardar la clave pública en un archivo PEM
-    with open(filename_public, 'wb') as f:
-        f.write(public_key.public_bytes(
+        public_key = private_key.public_key()
+        public_pem = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ))
+        )
+
+        with open('private.pem', 'wb') as private_file, open('public.pem', 'wb') as public_file:
+            private_file.write(private_pem)
+            public_file.write(public_pem)
+
+    except Exception as e:
+        raise Exception("Error al generar las claves rsa")
 
 
 # Definir las rutas de los archivos de claves
-SIGNING_KEY_FILE = os.getenv('SIGNING_KEY_NAME_FILE', 'signing_key.pem')
-VERIFYING_KEY_FILE = os.getenv('VERIFYING_KEY_NAME_FILE', 'verifying_key.pem')
+SIGNING_KEY_FILE = os.getenv('SIGNING_KEY_NAME_FILE', 'private.pem')
+VERIFYING_KEY_FILE = os.getenv('VERIFYING_KEY_NAME_FILE', 'public.pem')
 
 # Generar las claves si no existen
 if not os.path.exists(SIGNING_KEY_FILE) or not os.path.exists(VERIFYING_KEY_FILE):
-    generate_rsa_key(SIGNING_KEY_FILE, VERIFYING_KEY_FILE)
+    generate_rsa_key()
 
 # Configuraciones de Simple JWT
 SIMPLE_JWT = {
